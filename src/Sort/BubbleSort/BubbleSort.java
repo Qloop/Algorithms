b6a4bd9ee727ab9b4575dd1901df1dd0fe2333d8
Qloop <m18764231680@163.com>
2017-10-16 14:54:42 +0800

    优化冒泡排序;

--- src/Sort/BubbleSort/BubbleSort.java
+++ src/Sort/BubbleSort/BubbleSort.java
@@ -3,22 +3,30 @@ package Sort.BubbleSort;
 import Sort.CallTest;
 import Sort.SortTestHelper;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int n = 10;
+        int n = 10000;
         int[] arr = SortTestHelper.generateRandomArray(n, 0, n);
+        int[] arr2 = Arrays.copyOf(arr, arr.length);
         SortTestHelper.testSort("冒泡排序", new CallTest() {
             @Override
             public void test() {
                 sort(arr);
-                SortTestHelper.printArray(arr);
+//                SortTestHelper.printArray(arr);
+            }
+        });
+        SortTestHelper.testSort("优化的冒泡排序", new CallTest() {
+            @Override
+            public void test() {
+                sortAdvanced(arr2);
             }
         });
     }
 
-    public static void sort(int[] arr) {
-        int n = arr.length;
+    private static void sort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
@@ -26,5 +34,20 @@ public class BubbleSort {
                 }
             }
         }
+    }
+
+    private static void sortAdvanced(int[] arr) {
+        int n = arr.length;
+        int lastSwapIndex;
+        do {
+            lastSwapIndex = 0;
+            for (int i = 1; i < n; i++) {
+                if (arr[i - 1] > arr[i]) {
+                    SortTestHelper.swap(arr, i - 1, i);
+                    lastSwapIndex = i;
+                }
+            }
+            n = lastSwapIndex;
+        } while (lastSwapIndex > 0);
     }
 }
